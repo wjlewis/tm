3556d93903ba5eebfb2909aff98775c84b265d57
william j lewis <william.lewis112358@gmail.com>
2019-09-04 14:22:54 -0400

    Validate transitions when arrow is added

--- src/state-mgmt/middleware/validateTransitionDetails.ts
+++ src/state-mgmt/middleware/validateTransitionDetails.ts
@@ -6,7 +6,13 @@ import { duplicateTransitionDetails } from '../TransitionDetail';
 // transition details that share an arrow and have the same read symbol. These
 // represent nondeterministic transitions and should be marked as errors.
 export const validateTransitionDetails: Middleware = api => next => action => {
-  if (![A.ADD_TRANSITION_DETAIL, A.CHANGE_TRANSITION_DETAIL, A.DELETE_TRANSITION_DETAIL].includes(action.type)) return next(action);
+  const validateOn = [
+    A.ADD_TRANSITION_DETAIL,
+    A.CHANGE_TRANSITION_DETAIL,
+    A.DELETE_TRANSITION_DETAIL,
+    A.ADD_ARROW,
+  ];
+  if (!validateOn.includes(action.type)) return next(action);
 
   next(action);
   const duplicates = duplicateTransitionDetails(api.getState());
