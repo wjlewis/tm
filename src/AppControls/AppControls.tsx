eadfa13506afcd0e3af67c0a4437d9e0030698e8
william j lewis <william.lewis112358@gmail.com>
2019-09-06 08:00:19 -0400

    Improve top app bar
    I've increased the margins slightly, made the upload/download icons a
    little nicer, added title/help text, and added a few more links.

--- src/AppControls/AppControls.tsx
+++ src/AppControls/AppControls.tsx
@@ -7,12 +7,18 @@ class AppControls extends React.Component<AppControlsProps> {
   render() {
     return (
       <div className="app-controls">
-        <button className="app-controls__button app-controls__download-button">
-        </button>
-        <button className="app-controls__button app-controls__upload-button">
-        </button>
+        <div className="app-controls__buttons">
+          <button className="app-controls__button app-controls__download-button"
+                  title="download machine (JSON)">
+          </button>
+          <button className="app-controls__button app-controls__upload-button"
+                  title="upload machine (JSON)">
+          </button>
+        </div>
 
+        <a className="app-controls__link" href="#examples">Examples</a>
         <a className="app-controls__link" href="#tutorial">Tutorial</a>
+        <a className="app-controls__link" href="#guide">Guide</a>
         <a className="app-controls__link" href="#about">About</a>
         <a className="app-controls__link app-controls__source-link"
            href="https://github.com/wjlewis/tm"
