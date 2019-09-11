decdef8e45a59f417450df44b698884fdf0580b9
william j lewis <william.lewis112358@gmail.com>
2019-09-11 13:10:07 -0400

    Add unary->binary example

--- src/AppControls/examples/snapshots.ts
+++ src/AppControls/examples/snapshots.ts
@@ -1,9 +1,11 @@
 import copy1 from './copy-(single-symbol).json';
 import multiply from './multiply-(unary).json';
 import divBy2 from './divide-by-2-(unary).json';
+import unaryToBinary from './unary-to-binary-(lsb-first).json';
 
 export default [
   divBy2,
   copy1,
   multiply,
+  unaryToBinary,
 ];
