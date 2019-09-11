dcdff65407f298ebe93873fd6c82882e8182ccbb
william j lewis <william.lewis112358@gmail.com>
2019-09-11 13:42:13 -0400

    Improve A^n B^n C^n example
    There was no need for the separators (#), and the machine is
    significantly clarified without them.

--- src/AppControls/examples/snapshots.ts
+++ src/AppControls/examples/snapshots.ts
@@ -1,9 +1,11 @@
+import aNbNcN from './decide-a^n-b^n-c^n.json';
 import copy1 from './copy-(single-symbol).json';
 import multiply from './multiply-(unary).json';
 import divBy2 from './divide-by-2-(unary).json';
 import unaryToBinary from './unary-to-binary-(lsb-first).json';
 
 export default [
+  aNbNcN,
   divBy2,
   copy1,
   multiply,
