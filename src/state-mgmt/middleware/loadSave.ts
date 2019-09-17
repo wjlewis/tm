64dc79ee988ca261b872d39f5f39d190d6d2e283
william j lewis <william.lewis112358@gmail.com>
2019-09-17 17:05:40 -0400

    Load example for new users
    Now, when a user loads the app for the first time, they will be greeted
    with an example that they can immediately try out.

--- src/state-mgmt/middleware/loadSave.ts
+++ src/state-mgmt/middleware/loadSave.ts
@@ -1,12 +1,16 @@
 import { Middleware } from 'redux';
 import * as A from '../actions';
 import { getSnapshot } from '../auxiliary';
+import AnBnCnExample from '../../AppControls/examples/decide-a^n-b^n-c^n.json';
 
 export const loadSnapshot: Middleware = _ => next => action => {
   if (action.type !== A.LOAD_SNAPSHOT) return next(action);
   try {
     const saved = localStorage.getItem('machine');
-    if (!saved) return;
+    if (!saved) {
+      // This will give a new user something to play around with right away.
+      return next(A.installSnapshot(AnBnCnExample, false));
+    }
     const snapshot = JSON.parse(saved);
     next(A.installSnapshot(snapshot, false));
   } catch(_) {
